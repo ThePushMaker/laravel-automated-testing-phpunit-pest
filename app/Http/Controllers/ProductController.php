e7a3b6a8f52a68ad694e8f04659832b2539ed404
Martin Calderón <elmcwl@gmail.com>
2024-07-20 19:00:44 -0700

    feat: add laravel sail, products table, products view and displaying of products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ProductController extends Controller
+{
+    public function index(): View
+    {
+        $products = Product::all();
+
+        return view('products.index', compact('products'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::redirect('/dashboard', '/products')->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // products
+    // Route::redirect('/', 'products');
+
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+});
+
+require __DIR__.'/auth.php';
