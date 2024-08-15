bb34e9556efc14661bc7c842f1297f6c31b5fd5b
Martin Calderón <elmcwl@gmail.com>
2024-08-14 21:49:27 -0700

    feat: add is_admin middleware

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,4 +14,14 @@ class ProductController extends Controller
 
         return view('products.index', compact('products'));
     }
+    
+    public function create()
+    {
+        dd('create');
+    }
+    
+    public function store(Request $request)
+    {
+        // 
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,14 @@ Route::get('/', function () {
 
 // Route::redirect('/', '/products');
 
-Route::get('/dashboard', [ProductController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [ProductController::class, 'index'])->middleware(['verified'])->name('dashboard');
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
     
     Route::middleware('is_admin')->group(function () {
        Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
-       Route::post('products', [ProductController::class, 'store'])->name('products.store'); 
+       Route::post('products', [ProductController::class, 'store'])->name('products.store');
     });
     
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
