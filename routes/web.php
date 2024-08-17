f089f28f408111b82c7e52ad0b13876821b88937
Martin Calderón <elmcwl@gmail.com>
2024-08-16 17:01:26 -0700

    feat: add web routes to display edit product view and update product

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::middleware('auth')->group(function () {
     Route::middleware('is_admin')->group(function () {
        Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
        Route::post('products', [ProductController::class, 'store'])->name('products.store');
+       Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+       Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update');
     });
     
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
